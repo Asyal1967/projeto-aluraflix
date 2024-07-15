2a2fffda6135bef1825dcdc38693c7e713fa5df7
Laysa Numaire <laysa_numaire@hotmail.com>
2024-07-14 22:46:04 -0300

    adicionei uma lib de scrollbar, configurei o router e o bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import 'overlayscrollbars/overlayscrollbars.css';
+import './scss/styles.scss'
+import {
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
+import PaginaNaoEncontrada from './pages/404';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "*",
+    element: <PaginaNaoEncontrada />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <OverlayScrollbarsComponent defer>
+      <RouterProvider router={router} />
+    </OverlayScrollbarsComponent>
   </React.StrictMode>,
 )
