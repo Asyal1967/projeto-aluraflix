517890d2642b255a3887a997c337a1e24a7e25c8
Laysa Numaire <laysa_numaire@hotmail.com>
2024-07-14 23:57:50 -0300

    começada  a barra de navegação

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import './styles.scss'
+
+export default function BarraDeNavegacao() {
+  return (
+    <header className='p-4'>
+      <img src="/images/logo.png" alt="Logo Aluraflix" />
+    </header>
+  )
+}
