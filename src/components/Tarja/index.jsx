aa58b0c296d6cde330029268cae5907778eab98a
Laysa Numaire <laysa_numaire@hotmail.com>
2024-07-15 23:28:32 -0300

    add tarja

---
+++ src/components/Tarja/index.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import './styles.scss'
+
+export default function Tarja({ categoria, cor, tamanho }) {
+  return (
+    <div className={`rounded-4 bg-${cor} tarja display-${tamanho} fw-bold text-uppercase p-3`}>
+      {categoria}
+    </div>
+  )
+}
+
+Tarja.propTypes = {
+  categoria: PropTypes.string.isRequired,
+  cor: PropTypes.string.isRequired,
+  tamanho: PropTypes.number.isRequired
+}
