b08418c1d44de5f84fef936c54610043621845c9
Laysa Numaire <laysa_numaire@hotmail.com>
2024-07-16 00:47:13 -0300

    valor padrão para o caso do banner

--- src/components/Tarja/index.jsx
+++ src/components/Tarja/index.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import './styles.scss'
 
-export default function Tarja({ categoria, cor, tamanho }) {
+export default function Tarja({ categoria, cor, tamanho = 6 }) {
   return (
     <div className={`rounded-4 bg-${cor} tarja display-${tamanho} fw-bold text-uppercase p-3`}>
       {categoria}
