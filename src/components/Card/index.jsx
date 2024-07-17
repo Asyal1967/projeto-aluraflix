529b7a84ccee20e2d780a3743464cf9744601090
Laysa Numaire <laysa_numaire@hotmail.com>
2024-07-16 22:39:36 -0300

    card consertado

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,10 +1,11 @@
 import './styles.scss'
 import PropTypes from 'prop-types';
 import Lixeira from "../IconeLixeira";
+import Editar from '../IconeEditar';
 
 export default function Card({ video }) {
   return (
-    <div className="card rounded-3 cartao-video" {...{ categoria: video.categoria }}>
+    <div className="card cartao-video" {...{ categoria: video.categoria }}>
       <div className="container-imagem" {...{ categoria: video.categoria }}>
         <img
           src={video.imagem}
@@ -12,10 +13,14 @@ export default function Card({ video }) {
           className="card-img-top img-fluid"
         />
       </div>
-      <div className="card-body">
-        <button className="btn btn-dark">
-          <Lixeira className="icone-lixeira" />
-          Deletar
+      <div className="card-body d-flex justify-content-evenly">
+        <button className="btn">
+          <Lixeira className="icone-lixeira me-3" />
+          <span className='text-uppercase fw-bold'>Deletar</span>
+        </button>
+        <button className='btn'>
+          <Editar className="icone-lixeira me-3" />
+          <span className='text-uppercase fw-bold'>Editar</span>
         </button>
       </div>
     </div>
