f6597d96aab358ca6ff408f5b50cec0ec594a2b7
Laysa Numaire <laysa_numaire@hotmail.com>
2024-07-16 21:38:57 -0300

    tarja preparada para ser grande ou pequena

--- src/components/Tarja/index.jsx
+++ src/components/Tarja/index.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import './styles.scss'
 
-export default function Tarja({ categoria, cor, tamanho = 6 }) {
+export default function Tarja({ categoria, cor, tamanho = 'display-6' }) {
   return (
-    <div className={`rounded-4 bg-${cor} tarja display-${tamanho} fw-bold text-uppercase p-3`}>
+    <div className={`${tamanho.includes('display') ? 'rounded-4' : 'rounded-3'} bg-${cor} tarja ${tamanho} fw-bold text-uppercase ${tamanho.includes('display') ? 'p-3' : 'px-5 py-2 mt-4'}`}>
       {categoria}
     </div>
   )
