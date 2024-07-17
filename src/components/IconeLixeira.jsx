b4d553eb9254f2a771ae60ecda8f4387e76a5730
Laysa Numaire <laysa_numaire@hotmail.com>
2024-07-16 22:38:12 -0300

    add icones corretos

--- src/components/IconeLixeira.jsx
+++ src/components/IconeLixeira.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function Lixeira(props) {
   return (
     <svg width="26" height="28" viewBox="0 0 26 28" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
