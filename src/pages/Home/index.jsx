6cdffff5085ccc76a1a3ecf143026db0b898c348
Laysa Numaire <laysa_numaire@hotmail.com>
2024-07-16 21:39:21 -0300

    add componentes novos a pagina inicial

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,22 +1,31 @@
-import BarraDeNavegacao from '@components/BarraDeNavegacao'
-import Banner from '../../components/Banner';
-import './styles.scss'
-import { useLoaderData } from 'react-router-dom';
+import BarraDeNavegacao from "@components/BarraDeNavegacao";
+import Banner from "../../components/Banner";
+import "./styles.scss";
+import { useLoaderData } from "react-router-dom";
+import ListaDeCards from "../../components/ListaDeCards";
 
 function Home() {
-
   const videos = useLoaderData();
 
   return (
     <>
-    <header>
-      <BarraDeNavegacao />
-    </header>
-    <main className='container-fluid h-100 px-0'>
-        <Banner video={videos[0]} />
-    </main>
+      <header>
+        <BarraDeNavegacao />
+      </header>
+      <main className="container-fluid h-100 px-0">
+        {videos && <Banner video={videos[0]} />}
+        <section className="container-fluid px-5">
+          {videos && (
+            <>
+            <ListaDeCards videos={videos} categoria='Front end' />
+            <ListaDeCards videos={videos} categoria='Back end' />
+            <ListaDeCards videos={videos} categoria='Mobile' />
+            </>
+            )}
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
 export default Home;
